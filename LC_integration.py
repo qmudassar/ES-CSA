af1fa76129b80b773089c62c16c045b6e32db215
Qasim Mudassar <156016116+qmudassar@users.noreply.github.com>
2025-02-20 14:18:02 -0800

    Updated LLM_integration.py

--- LC_integration.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.llms import Ollama
-
-# LangChain w/ Phi-2
-
-llm = Ollama(model="phi-2")
-
-# Test Query
-response = llm.invoke("Explain the benefits of postpaid mobile plans.")
-print(response)
